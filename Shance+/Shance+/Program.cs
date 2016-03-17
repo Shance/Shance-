746499268c25744fae00971fbeef29d94c7b7021
Alexsandr <schurik3110@yandex.ru>
2016-03-17 08:54:37 +0800

    Hello vord!

---
+++ Shance+/Shance+/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shance_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello vord!");
+            Console.ReadLine();
+        }
+    }
+}
